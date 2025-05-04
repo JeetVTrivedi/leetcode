440a71e2965f51e1ef56fe59fcc510fcce581359
JeetVTrivedi <137861272+JeetVTrivedi@users.noreply.github>
2025-05-04 16:43:29 +0530

    12.py

---
+++ tufpattern/12.py
@@ -0,0 +1,11 @@
+n= 4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end='')
+    for a in range(n*2-i*2):
+        print(' ',end='')
+    for j in range(1,i+1):
+        print(i-j+1,end='')
+        
+    print('')
+    
