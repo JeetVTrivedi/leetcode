8ba005a44754d8ca5a97f1f7ab2ad71f5bc79a56
JeetVTrivedi <jeettrivedi2004@gmail.com>
2025-05-06 18:06:05 +0530

    count digits

--- tuf/basic maths/count digits.py
+++ tuf/basic maths/count digits.py
@@ -1,3 +1,6 @@
+import math
+
+#time =o(log10(number))
 n=142356789
 num=n
 count=0
@@ -5,4 +8,8 @@ while(num>0):
     num//=10
     count+=1
 print(count)
-    
+
+#time=o(1)
+num=n
+count=0
+print(int(math.log10(num))+1)
